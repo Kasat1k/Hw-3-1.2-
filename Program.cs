d3ac217b61cb0192f0e713574ef6d159a64b877d
kasat <kasat@DESKTOP-DOQKB49>
2022-11-10 21:58:05 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введіть число");
+string str;
+int n;
+str = Console.ReadLine();
+n = Convert.ToInt32(str);
+Console.WriteLine("Ви ввели число " + n);
